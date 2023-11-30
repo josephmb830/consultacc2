b0bef8568983f6503120bfdeab047a3e59e0d003
josephmb830 <josephmoran@duck.com>
2023-11-29 21:44:59 -0500

    filtros principales

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+// Componente de filtros principales
+const anioOptions = ['Todos los Años', '1996', '1997', '1998', '1999', '2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021', '2022', '2023'];
+
+// Lógica para generar el dropdown de años dinámicamente
+const anioDropdownContainer = document.getElementById('anioDropdownContainer');
+const dropdownHTML = `
+  <select id="anioDropdown">
+    ${anioOptions.map(option => `<option value="${option}">${option}</option>`).join('')}
+  </select>
+`;
+anioDropdownContainer.innerHTML = dropdownHTML;
+
+// fin de filtros principales
+
 // Contenido del archivo index.js
 let contribuyente = { nombre: 'Nombre del Contribuyente', direccion: 'Dirección del Contribuyente' };
 
